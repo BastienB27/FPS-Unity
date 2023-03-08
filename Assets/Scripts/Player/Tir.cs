7cbc26146c1d02a7f438648bb23c2dc7e02641f2
Floria <florian.richet2004@gmail.com>
2023-03-08 10:53:47 +0100

    V2–Impacte

--- Assets/Scripts/Player/Tir.cs
+++ Assets/Scripts/Player/Tir.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Tir : MonoBehaviour
@@ -7,15 +6,20 @@ public class Tir : MonoBehaviour
 
     public float degat = 10f;
     public float portée = 100f;
+    public float cadence = 15f;
 
     public Camera fpsCam;
     public ParticleSystem Départ_du_coup;
+    public GameObject impacte;
+
+    private float cadence_tir = 0f;
 
     void Update()
     {
 
-        if(Input.GetKey(KeyCode.Mouse0))
+        if(Input.GetKey(KeyCode.Mouse0) && Time.time >= cadence_tir)
         {
+            cadence_tir = Time.time + 1f/cadence;
             Shoot();
         }   
     }
@@ -33,6 +37,9 @@ public class Tir : MonoBehaviour
             if (cible != null){ // Si le truc touché à un script 
                 cible.Touché(degat); // degat ta capterrrrrrrrr
             } 
+
+            GameObject impactGO = Instantiate(impacte, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.1f);
         }
          
     }
